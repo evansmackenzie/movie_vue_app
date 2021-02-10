3101bd628c51973ef14daae1441f376018f4141b
mackenzie <evans.mackenzie@gmail.com>
2021-02-09 19:25:20 -0500

    adds initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    
+    <div v-for="movie in movies"  v-bind:key="movie.id">
+      <h1>Title: {{ movie.title }}</h1>
+      <p>plot: {{ movie.plot }}</p>
+      <p>year: {{ movie.year }}</p>
+      <p>director: {{ movie.director }}</p>
+      <p>english? {{ movie.english }}</p>
+      <p>genres: {{ movie.genres }}</p>
+    </div>
+
+    <div>
+      <button v-on:click="createMovie">Add Movie</button>
+
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/api/movies").then((response) => {
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: "Seven Samurai",
+        year: 1957,
+        plot: "Seven samurai protect a village.",
+        director: "Akira Kurosawa",
+        english: false,
+      };
+      axios
+        .post("http://localhost:3000/api/movies", params)
+        .then((response) => {
+          this.movies.push(response.data);
+        });
+    },
+  },
 };
 </script>
